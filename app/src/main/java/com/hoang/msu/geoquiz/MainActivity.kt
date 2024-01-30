0679a1171703ba383f4d88513a1648aaee7924ec
Caylipp <philipphoang3005@gmail.com>
2024-01-30 11:34:47 -0600

    -Homework 2 done

--- app/src/main/java/com/hoang/msu/geoquiz/MainActivity.kt
+++ app/src/main/java/com/hoang/msu/geoquiz/MainActivity.kt
@@ -3,24 +3,31 @@ package com.hoang.msu.geoquiz
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
 import android.widget.Button
-
 import com.google.android.material.snackbar.Snackbar
+import com.hoang.msu.geoquiz.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var trueButton : Button
-    private lateinit var falseButton : Button
+    private lateinit var binding : ActivityMainBinding
+
+
+    private val questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true))
+
+    private var currentIndex = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        trueButton = findViewById(R.id.true_button)
-        falseButton = findViewById(R.id.false_button)
-
-        trueButton.setOnClickListener{
+        binding.trueButton.setOnClickListener{
             val snackbar = Snackbar.make(
                 it,
                 "Correct",
@@ -29,7 +36,7 @@ class MainActivity : AppCompatActivity() {
             snackbar.show()
         }
 
-        falseButton.setOnClickListener{
+        binding.falseButton.setOnClickListener{
             val snackbar = Snackbar.make(
                 it,
                 "Incorrect",
@@ -40,5 +47,18 @@ class MainActivity : AppCompatActivity() {
             snackbar.show()
         }
 
+        binding.nextButton.setOnClickListener{
+            currentIndex = (currentIndex + 1) % questionBank.size
+            val questionTextResId = questionBank[currentIndex].textResId
+            binding.questionTextview.setText(questionTextResId)
+            updateQuestion()
+        }
+        updateQuestion() //necessary??
+
+    }
+
+    private fun updateQuestion() {
+        val questionTextResId = questionBank[currentIndex].textResId
+        binding.questionTextview.setText(questionTextResId)
     }
 }
---
+++ app/src/main/java/com/hoang/msu/geoquiz/Question.kt
@@ -0,0 +1,9 @@
+package com.hoang.msu.geoquiz
+
+import androidx.annotation.StringRes
+
+data class Question(@StringRes val textResId: Int, val answer: Boolean){
+
+
+
+}
