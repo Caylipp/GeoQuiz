feb06b6892e5bdf371d36d5d7bb7a5330bcc929e
Caylipp <philipphoang3005@gmail.com>
2024-02-08 16:48:57 -0600

    HW3: Exercise 1: -added loging

--- app/src/main/java/com/hoang/msu/geoquiz/MainActivity.kt
+++ app/src/main/java/com/hoang/msu/geoquiz/MainActivity.kt
@@ -1,11 +1,9 @@
 package com.hoang.msu.geoquiz
 
-import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
-import com.google.android.material.snackbar.Snackbar
 import com.hoang.msu.geoquiz.databinding.ActivityMainBinding
 
 private const val TAG = "MainActivity"
@@ -72,5 +70,26 @@ class MainActivity : AppCompatActivity() {
         }
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT)
             .show()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart() called")
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume() called")
+    }
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause() called")
+    }
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop() called")
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() called")
     }
 }
