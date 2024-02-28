bced37a803dc1a6e3b3a33c878d64d6dfb58550f
Caylipp <philipphoang3005@gmail.com>
2024-02-27 18:00:14 -0600

    Week7 -add cheat

--- app/src/main/java/com/hoang/msu/geoquiz/MainActivity.kt
+++ app/src/main/java/com/hoang/msu/geoquiz/MainActivity.kt
@@ -1,20 +1,33 @@
 package com.hoang.msu.geoquiz
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.activity.viewModels
 import com.hoang.msu.geoquiz.databinding.ActivityMainBinding
+import android.content.Intent
+import androidx.activity.result.contract.ActivityResultContracts
 
 private const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
-
     private var score = 0
-
     private val quizViewModel : QuizViewModel by viewModels()
+
+    private val cheatLauncher = registerForActivityResult(
+
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+// Handle the result
+        if (result.resultCode == Activity.RESULT_OK) {
+            quizViewModel.isCheater =
+                result.data?.getBooleanExtra(EXTRA_ANSWER_SHOWN, false) ?: false
+        }
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,6 +42,15 @@ class MainActivity : AppCompatActivity() {
 
         binding.falseButton.setOnClickListener{
             checkAnswer(false)
+        }
+
+        binding.cheatButton.setOnClickListener(){
+            //val intent = Intent(this, CheatActivity::class.java)
+            val answerIsTrue = quizViewModel.currentQuestionAnswer
+            val intent = CheatActivity.newIntent(this@MainActivity, answerIsTrue)
+            //startActivity(intent)
+            cheatLauncher.launch(intent)
+
         }
 
         binding.nextButton.setOnClickListener{
@@ -59,11 +81,17 @@ class MainActivity : AppCompatActivity() {
     private fun checkAnswer(userAnswer: Boolean) {
         disableInputBtn()
         val correctAnswer = quizViewModel.currentQuestionAnswer
-        val messageResId = if (userAnswer == correctAnswer) {
-            score++
-            R.string.correct_toast
-        } else {
-            R.string.incorrect_toast
+
+//        val messageResId = if (userAnswer == correctAnswer) {
+//            score++
+//            R.string.correct_toast
+//        } else {
+//            R.string.incorrect_toast
+//        }
+        val messageResId = when {
+            quizViewModel.isCheater -> R.string.judgment_toast
+            userAnswer == correctAnswer -> R.string.correct_toast
+            else -> R.string.incorrect_toast
         }
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT)
             .show()
